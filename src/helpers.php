fcae985b784bbac63fd935960524c44865a3a416
Daniel Sum <daniel@cherrypulp.com>
2014-10-22 17:15:41 +0200

    - stabilize Confide And Entrust version

--- src/helpers.php
+++ src/helpers.php
@@ -1,9 +1,8 @@
 <?php
+
 /**
  * Helpers used in Arxmin
  *
  * @project : arx-contrib
  * @author : Daniel Sum <daniel@cherrypulp.com>
  */
-
-\Arx\classes\Utils::alias('__', 'Arxmin\\LangModel::get');
